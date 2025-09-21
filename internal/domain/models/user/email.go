ac08d38e4f924794111c6507bfcf486835721578
Nahaq789 <naha022677@icloud.com>
2025-09-22 08:36:14 +0900

    email

---
+++ internal/domain/models/user/email.go
@@ -0,0 +1,31 @@
+package user
+
+import (
+	"errors"
+	"regexp"
+)
+
+type Email struct {
+	value string
+}
+
+func NewEmail(v string) (*Email, error) {
+	if err := validateEmail(v); err != nil {
+		return nil, err
+	}
+	return &Email{value: v}, nil
+}
+
+func validateEmail(v string) error {
+	emailRegex := `^[a-zA-Z0-9!#$%&'*+/=?^_` + "`" + `{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_` + "`" + `{|}~-]+)*@[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?)+$`
+	re := regexp.MustCompile(emailRegex)
+
+	if !re.MatchString(v) {
+		return errors.New("メールアドレスの形式が正しくないです。")
+	}
+	return nil
+}
+
+func (e *Email) GetValue() string {
+	return e.value
+}
