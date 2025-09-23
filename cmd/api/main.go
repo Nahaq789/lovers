0c12b7025524121c68bce595fb17562742c8a9f2
Nahaq789 <naha022677@icloud.com>
2025-09-23 11:06:25 +0900

    graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"log/slog"
 	"lovers/internal/shared/infrastructure/logger"
 	"net/http"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -19,5 +25,27 @@ func main() {
 
 	r := gin.Default()
 	Router(r)
-	http.ListenAndServe(":8080", r)
+	server := &http.Server{Addr: ":8080", Handler: r.Handler()}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	defer wg.Wait()
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		<-c
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			logger.InfoContext(ctx, "HTTP Server Shutdown", "error", err)
+		}
+		wg.Done()
+	}()
+
+	if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		logger.ErrorContext(context.Background(), "HTTP server ListenAndServe", "error", err)
+	}
 }
--- cmd/api/router.go
+++ cmd/api/router.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log/slog"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,6 +10,7 @@ import (
 func Router(r gin.IRouter) {
 	v1 := r.Group("api/v1")
 	v1.GET("/ping", func(c *gin.Context) {
+		time.Sleep(5 * time.Second)
 		slog.InfoContext(c.Request.Context(), "良い子のみんな〜")
 		c.JSON(200, gin.H{
 			"message": "pong",
