3161764bb54632bc08bf80d97ec98dea240721ae
Nahaq789 <naha022677@icloud.com>
2025-09-28 18:39:33 +0900

    cognito client

--- internal/shared/infrastructure/aws/cognito.go
+++ internal/shared/infrastructure/aws/cognito.go
@@ -2,20 +2,28 @@ package aws
 
 import (
 	"context"
+	"log/slog"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/cognitoidentityprovider"
 )
 
 type CognitoClient struct {
+	logger slog.Logger
 	client cognitoidentityprovider.Client
 }
 
-func InitCognitoClient(ctx context.Context) (*CognitoClient, error) {
+func InitCognitoClient(ctx context.Context, l slog.Logger) (*CognitoClient, error) {
 	sdkConfig, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
-		return nil, nil
+		l.ErrorContext(ctx, "failed init cognito client", err)
+		return nil, err
 	}
 
-	return nil, nil
+	client := cognitoidentityprovider.NewFromConfig(sdkConfig)
+
+	return &CognitoClient{
+		logger: l,
+		client: *client,
+	}, nil
 }
