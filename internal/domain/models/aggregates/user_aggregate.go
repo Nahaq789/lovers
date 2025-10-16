04ea1849a849ebb82679bf17a8ea11031d1b86f4
Nahaq789 <naha022677@icloud.com>
2025-10-17 08:31:02 +0900

    fix: aggregate

--- internal/domain/models/aggregates/user_aggregate.go
+++ internal/domain/models/aggregates/user_aggregate.go
@@ -3,10 +3,19 @@ package aggregates
 import (
 	"lovers/internal/domain/models/user/email"
 	"lovers/internal/domain/models/user/password"
+	userid "lovers/internal/domain/models/user/user_id"
 )
 
 type UserAggregate struct {
-	userId   string
+	userId   userid.UserId
 	email    email.Email
 	password password.Password
+}
+
+func NewUserAggregate(id userid.UserId, email email.Email, password password.Password) *UserAggregate {
+	return &UserAggregate{
+		userId:   id,
+		email:    email,
+		password: password,
+	}
 }
