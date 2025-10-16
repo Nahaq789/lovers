f9ca52c991d9273a02711d380d22efd8a95725d7
Nahaq789 <naha022677@icloud.com>
2025-10-17 08:31:13 +0900

    feat: repo

--- internal/domain/repositories/user_repository.go
+++ internal/domain/repositories/user_repository.go
@@ -1,7 +1,13 @@
 package repositories
 
-import "context"
+import (
+	"context"
+	"lovers/internal/domain/models/aggregates"
+	"lovers/internal/domain/models/entity"
+	userid "lovers/internal/domain/models/user/user_id"
+)
 
 type UserRepository interface {
-	Register(ctx context.Context)
+	Register(ctx context.Context, user aggregates.UserAggregate) error
+	GetUser(ctx context.Context, userId userid.UserId) (*entity.UserEntity, error)
 }
