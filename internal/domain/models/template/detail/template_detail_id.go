932b3b1ec3f72a97dde52da306fa12ec4088a935
Nahaq789 <naha022677@icloud.com>
2025-12-22 16:25:49 +0900

    feat: テンプレート名とdetail id #4

---
+++ internal/domain/models/template/detail/template_detail_id.go
@@ -0,0 +1,36 @@
+package detail
+
+import (
+	"github.com/google/uuid"
+	valueObjectUuid "lovers/internal/domain/models/value_objects/uuid"
+)
+
+type TemplateDetailId struct {
+	value uuid.UUID
+}
+
+func NewTemplateDetailId() (TemplateDetailId, error) {
+	return newTemplateDetailIdWithGenerator(valueObjectUuid.DefaultGenerator)
+}
+
+func newTemplateDetailIdWithGenerator(generator valueObjectUuid.UUIDGenerator) (TemplateDetailId, error) {
+	u, err := generator()
+	if err != nil {
+		return TemplateDetailId{}, err
+	}
+
+	return TemplateDetailId{value: u}, nil
+}
+
+func NewTemplateDetailIdFromString(s string) (TemplateDetailId, error) {
+	v, err := uuid.Parse(s)
+	if err != nil {
+		return TemplateDetailId{}, err
+	}
+
+	return TemplateDetailId{value: v}, nil
+}
+
+func (t TemplateDetailId) GetValue() string {
+	return t.value.String()
+}
