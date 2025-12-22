990f9d1555d41b6daf728f7c4c50a6f5b79fa7f4
Nahaq789 <naha022677@icloud.com>
2025-12-22 17:06:46 +0900

    feat: グループ作成 #4

---
+++ internal/domain/models/group/group_name.go
@@ -0,0 +1,19 @@
+package group
+
+import "errors"
+
+type GroupName struct {
+	value string
+}
+
+func NewGroupName(v string) (GroupName, error) {
+	if len(v) > 20 {
+		return GroupName{}, errors.New("グループ名は20文字以内にしてください。")
+	}
+
+	return GroupName{value: v}, nil
+}
+
+func (g GroupName) GetValue() string {
+	return g.value
+}
