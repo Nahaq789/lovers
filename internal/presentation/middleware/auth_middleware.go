39763407ea5a6c680f8eff66e60a2c1dabb344d5
Nahaq789 <naha022677@icloud.com>
2026-01-09 19:07:45 +0900

    一旦コミット #4

---
+++ internal/presentation/middleware/auth_middleware.go
@@ -0,0 +1,14 @@
+package middleware
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		fmt.Println("hogehoge")
+		c.Next()
+	}
+}
