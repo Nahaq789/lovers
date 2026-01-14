3247cb218bfdde2b092a4e95f63c5f2bfcfa951c
Nahaq789 <naha022677@icloud.com>
2026-01-14 14:20:13 +0900

    認証ミドルウェア仮作成 #4

--- internal/presentation/middleware/auth_middleware.go
+++ internal/presentation/middleware/auth_middleware.go
@@ -1,14 +1,15 @@
 package middleware
 
 import (
-	"fmt"
+	"lovers/internal/shared/infrastructure/security/userid"
 
 	"github.com/gin-gonic/gin"
 )
 
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		fmt.Println("hogehoge")
+		ctx := userid.WithContext(c.Request.Context(), "123e4567-e89b-12d3-a456-426614174000")
+		c.Request = c.Request.WithContext(ctx)
 		c.Next()
 	}
 }
