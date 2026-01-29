229a7514d7255553554bdc307d6549035c59418d
Nahaq789 <naha022677@icloud.com>
2026-01-30 08:25:55 +0900

    feat: グループメンバー追加 #4

--- internal/infrastructure/repositories/group_repository.go
+++ internal/infrastructure/repositories/group_repository.go
@@ -2,9 +2,11 @@ package repositories
 
 import (
 	"context"
+	"fmt"
 	"lovers/internal/domain/models/aggregates/group"
 	"lovers/internal/shared/infrastructure/db"
 	"lovers/internal/shared/infrastructure/logger"
+	"strings"
 )
 
 type GroupRepositoryImpl struct {
@@ -17,19 +19,58 @@ func NewGroupRepository(d *db.DbClient) *GroupRepositoryImpl {
 
 func (g *GroupRepositoryImpl) Create(ctx context.Context, group group.GroupAggregate) error {
 	l := logger.FromContext(ctx)
-	query := `insert into "group" (group_id, created_by, group_name, created_at, updated_at) values ($1, $2, $3, $4, $5)`
 	c := g.db.GetClient()
-	_, err := c.ExecContext(ctx, query,
+
+	tx, txErr := c.BeginTx(ctx, nil)
+	if txErr != nil {
+		l.ErrorContext(ctx, "failed begin transaction", "error", txErr)
+		return txErr
+	}
+	defer tx.Rollback()
+
+	groupQuery := `insert into "group" (group_id, created_by, group_name, created_at, updated_at) values ($1, $2, $3, $4, $5)`
+	_, groupErr := c.ExecContext(ctx, groupQuery,
 		group.GetGroupId().GetValue(),
 		group.GetCreatedBy().GetValue(),
 		group.GetGroupName().GetValue(),
 		group.GetCreatedAt().GetValue(),
 		group.GetUpdatedAt().GetValue(),
 	)
-	if err != nil {
-		l.ErrorContext(ctx, "failed to create group", "error", err)
-		return err
+	if groupErr != nil {
+		l.ErrorContext(ctx, "failed to create group", "error", groupErr)
+		return groupErr
 	}
 
-	return nil
+	members := group.GetMembers()
+	if len(members) == 0 {
+		l.WarnContext(ctx, "メンバーが0圏のため、group_memberへのインサートをスキップします。")
+		return nil
+	}
+
+	memberQuery := `insert into "group_member" (group_member_id, group_id, user_id, created_at) values `
+	values := []interface{}{}
+	placeholders := []string{}
+
+	for i, m := range members {
+		offset := i * 4
+		placeholders = append(placeholders,
+			fmt.Sprintf("($%d, $%d, $%d, $%d)", offset+1, offset+2, offset+3, offset+4),
+		)
+		values = append(values,
+			m.GetGroupMemberId().GetValue(),
+			m.GetGroupId().GetValue(),
+			m.GetUserId().GetValue(),
+			group.GetCreatedAt(),
+		)
+	}
+
+	memberQuery += strings.Join(placeholders, ", ")
+
+	_, memberErr := c.ExecContext(ctx, memberQuery, values...)
+	if memberErr != nil {
+		l.ErrorContext(ctx, "failed to add group member", "error", memberErr)
+		return memberErr
+	}
+
+	return tx.Commit()
 }
