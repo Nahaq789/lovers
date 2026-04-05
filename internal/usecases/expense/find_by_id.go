866118b0d7fffdb891a230c8510fc9d1e85d6756
Nahaq789 <naha022677@icloud.com>
2026-04-05 10:48:50 +0900

    一旦コミット

--- internal/usecases/expense/find_by_id.go
+++ internal/usecases/expense/find_by_id.go
@@ -2,8 +2,11 @@ package expense
 
 import (
 	"context"
+	"lovers/internal/domain/models/expense/expenseid"
+	"lovers/internal/domain/models/user/userid"
 	"lovers/internal/domain/repositories"
 	"lovers/internal/shared/infrastructure/logger"
+	"lovers/internal/usecases/dto/expense/response"
 )
 
 type ExpenseFindById struct {
@@ -16,9 +19,27 @@ func NewExpenseFindById(er repositories.ExpenseRepository) *ExpenseFindById {
 	}
 }
 
-func (ef *ExpenseFindById) Execute(ctx context.Context, expenseId) error {
+func (ef *ExpenseFindById) Execute(ctx context.Context, expenseId, targetUserId string) (*response.ExpenseResponse, error) {
 	l := logger.FromContext(ctx)
 	l.InfoContext(ctx, "明細取得処理を開始します。")
 
-	return nil
+	e, err := expenseid.NewExpenseIdFromString(expenseId)
+	if err != nil {
+		l.ErrorContext(ctx, "明細IDの生成に失敗しました。", "error", err)
+		return nil, err
+	}
+
+	t, err := userid.NewUserIdFromString(targetUserId)
+	if err != nil {
+		l.ErrorContext(ctx, "ユーザIDの生成に失敗しました。", "error", err)
+		return nil, err
+	}
+
+	aggregate, err := ef.expenseRepository.FindById(ctx, e, t)
+	if err != nil {
+		l.ErrorContext(ctx, "明細の取得に失敗しました。", "error", err)
+		return nil, err
+	}
+
+	return nil, nil
 }
