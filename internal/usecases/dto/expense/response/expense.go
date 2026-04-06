e8fab879c68f301b0331aac6ae4707d24bf29c65
Nahaq789 <naha022677@icloud.com>
2026-04-06 10:50:50 +0900

    レスポンス作成 #4

--- internal/usecases/dto/expense/response/expense.go
+++ internal/usecases/dto/expense/response/expense.go
@@ -1,5 +1,22 @@
 package response
 
+import "time"
+
 type ExpenseResponse struct {
-	
+	ExpenseId    string         `json:"expense_id"`
+	GroupId      string         `json:"group_id"`
+	CategoryId   string         `json:"category_id"`
+	Total        int64          `json:"total"`
+	Nominal      string         `json:"nominal"`
+	PaymentDate  time.Time      `json:"payment_date"`
+	Description  string         `json:"description"`
+	DeletedAt    time.Time      `json:"deleted_at"`
+	CreatedAt    time.Time      `json:"created_at"`
+	UpdatedAt    time.Time      `json:"updated_at"`
+	PaymentUsers []PaymentUsers `json:"payment_users"`
+}
+
+type PaymentUsers struct {
+	UserId string `json:"user_id"`
+	Amount int64  `json:"amount"`
 }
